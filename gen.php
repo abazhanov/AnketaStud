47205c16e4f41634d964f8b98026f5f8c6b1b91b
WildMan <wildmanspam@gmail.com>
2017-05-15 16:49:15 +0300

    Добавил генерацию паролей

--- gen.php
+++ gen.php
@@ -21,41 +21,21 @@
     <div class="container">
           <h1>Преподаватель глазами студентов</h1>
 
-      <?php
-      header("Content-Type: text/html; charset=cp1251");
+      <?php //Блок подключения к БД
+        header("Content-Type: text/html; charset=cp1251");
+        $UID="viewer"; 
+        $PWD="qaz123"; 
+        $serverName = "172.17.3.7"; 
+        $connectionInfo = array( "Database"=>"HS", "UID"=>"$UID", "PWD"=>"$PWD"); 
+        $conn = sqlsrv_connect( $serverName, $connectionInfo); 
+        if(!$conn) die(print_r (sqlsrv_errors(),true));    
+          $request="SELECT oid, kod FROM gruppa WHERE sub<>1 ORDER BY kod"; //Текст запроса
+          $res=sqlsrv_query($conn, $request); //Выполнение запроса
+      ?>
       
-      $UID="viewer"; 
-      $PWD="qaz123"; 
-      $serverName = "172.17.3.7"; 
-      $connectionInfo = array( "Database"=>"HS", "UID"=>"$UID", "PWD"=>"$PWD"); 
-      $conn = sqlsrv_connect( $serverName, $connectionInfo); 
-
-      if(!$conn) die(print_r (sqlsrv_errors(),true));    
-
-      $request="SELECT oid, kod FROM gruppa WHERE sub<>1 ORDER BY kod";
-      $res=sqlsrv_query($conn, $request);
-      //$row=sqlsrv_fetch_object($res);
-      ?>
-
-
-      <!-- Single button -->
-      <div class="btn-group">
-        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Выберете группу <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu">
-    
-      <?php
-      while( $obj = sqlsrv_fetch_object($res)) {
-        echo "<li><a href=\"sel_prep.php?id=".$obj->oid."\">".$obj->kod."</a></li>";
-        //echo $obj->oid."---";
-       
-      }
-      ?>
-        </ul>
-      </div>
-
-      <form action="generate.php" method="get">
+      <div><!-- Форма для запроса количества создаваемых логинов и для какой группы -->
+        <h3>Создание логинов и паролей для пользователей</h3>
+        <form action="generate.php" method="get">
           <p>Введите количество создаваемых пользователей (логины):<br><input type="number" name="NumberOfUser"></p>
           <p><select size="10" multiple name="Gruppa">
             <?php
@@ -66,10 +46,8 @@
           </select></p>
           <p><input type="submit" value="Создать"></p>
         </form>
-
-
+      </div>
     </div>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script>
