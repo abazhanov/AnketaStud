a067c5dee3ddad12b872ee98dbdd6a295beb47b2
WildMan <wildmanspam@gmail.com>
2017-05-23 09:26:23 +0300

    ВАЖНО! Переработал файлы с использованием include

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="windows-1251">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Преподаватель глазами студентов</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/dopstyle.css" rel="stylesheet">
+     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  </head>
+  <body>
+      <?php
+        include 'connect_sql.php';
+      ?>
