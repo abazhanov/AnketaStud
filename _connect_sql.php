b6289d466879712227e30236f459b6ee466fbd9d
WildMan <wildmanspam@gmail.com>
2017-07-11 10:51:24 +0300

    Убрал лишние фалы в результате пробы теста

--- _connect_sql.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: text/html; charset=cp1251");
-        $UID="viewer"; 
-        $PWD="qaz123"; 
-        $serverName = "172.17.3.7"; 
-        $connectionInfo = array( "Database"=>"HS_FOR_TEST", "UID"=>"$UID", "PWD"=>"$PWD"); 
-        $conn = sqlsrv_connect( $serverName, $connectionInfo); 
-        if(!$conn) die(print_r (sqlsrv_errors(),true));    
-?>
